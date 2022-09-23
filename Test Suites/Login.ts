ec04686276bad928670f80c74751beae7d9f20aa
Dragota <cristian.dragota@betterqa.co>
2022-09-23 18:01:00 +0300

    test suites

---
+++ Test Suites/Login.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>52a88c2b-2756-41ed-88cc-22f708de9a99</testSuiteGuid>
+</TestSuiteEntity>
